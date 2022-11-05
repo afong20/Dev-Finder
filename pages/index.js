1c317b99c4863c7e6cb5b39aa4a3fd2d01259127
Aaron Fong <afong9@nait.ca>
2022-11-05 16:46:32 -0600

    basic setup finished

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
- 
+import Head from 'next/head'
+
 export default function Home() {
   return (
-        
- 
-          <header >
-             <h1>Next.js</h1>
-             <p>next + tailwind starter kit</p>
-          </header>
-          
-     
+          <>
+            <Head>
+              <meta name='description' content='Find the best devs for your business.' />
+              <title>Dev Finders</title>
+            </Head>
+            <header className='text-center py-[4rem] bg-zinc-300'>
+              <img className='w-[98px] mx-auto' src='favicon.svg' alt='Dev Finders'/>
+              <h1 className='text-5xl text-slate-600'>Dev Finders</h1>
+              <p className='text-2xl my-[1rem] text-slate-600'>Finding devs perfect for your business.</p>
+            </header>       
+          </>
   )
 }
